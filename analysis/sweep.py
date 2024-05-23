cc3764c7af29ee0486b708eb66352553b0a0b2be
michiel <m.s.nikken@student.utwente.nl>
2024-05-23 21:33:17 +0200

    Sweep on kinematic scale

--- analysis/sweep.py
+++ analysis/sweep.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     mode = os.environ.get("LOGGING", "online")
 
     sweep_config = {
-        'method': 'bayes'
+        'method': 'grid'
     }
 
     metric = {
@@ -25,22 +25,25 @@ if __name__ == '__main__':
     parameters_dict = {k: {'value': v} for k, v in parameters_dict.items()}
     # # Select parameters to sweep
     parameters_dict.update({
+        # 'kinematic_scale': {
+        #     'distribution': 'q_log_uniform_values',
+        #     'max': 1001,
+        #     'min': 0.1,
+        #     'q': 10
+        # },
+        # 'max_kin_weight': {
+        #     'distribution': 'q_log_uniform_values',
+        #     'max': 10000,
+        #     'min': 1,
+        #     'q': 10
+        # },
         'kinematic_scale': {
-            'distribution': 'q_log_uniform_values',
-            'max': 1001,
-            'min': 0.1,
-            'q': 10
-        },
-        'max_kin_weight': {
-            'distribution': 'q_log_uniform_values',
-            'max': 10000,
-            'min': 1,
-            'q': 10
-        },
+            'values': [100, 500, 1000, 10000]
+        }
     })
     sweep_config['parameters'] = parameters_dict
     sweep_config['name'] = 'kinloss_sweep'
-    sweep_id = wandb.sweep(sweep_config, project="diffusion_sweep")
+    sweep_id = wandb.sweep(sweep_config, project="diffusion")
 
     def train(config=None):
         with wandb.init(project="diffusion", config=config, mode=mode):
