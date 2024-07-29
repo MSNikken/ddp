076badef45323c3fc3c4b144dea7f3f1fc01824f
michiel <m.s.nikken@student.utwente.nl>
2024-07-29 16:55:40 +0200

    Move construction of config objects to utils

--- diffuser/utils/serialization.py
+++ diffuser/utils/serialization.py
@@ -6,7 +6,11 @@ import pdb
 
 from collections import namedtuple
 
+from .config import Config
+from .training import Trainer
+
 DiffusionExperiment = namedtuple('Diffusion', 'dataset renderer model diffusion ema trainer epoch')
+
 
 def mkdir(savepath):
     """
@@ -18,6 +22,7 @@ def mkdir(savepath):
     else:
         return False
 
+
 def get_latest_epoch(loadpath):
     states = glob.glob1(os.path.join(*loadpath), 'state_*')
     latest_epoch = -1
@@ -26,12 +31,14 @@ def get_latest_epoch(loadpath):
         latest_epoch = max(epoch, latest_epoch)
     return latest_epoch
 
+
 def load_config(*loadpath):
     loadpath = os.path.join(*loadpath)
     config = pickle.load(open(loadpath, 'rb'))
     print(f'[ utils/serialization ] Loaded config from {loadpath}')
     print(config)
     return config
+
 
 def load_diffusion(*loadpath, epoch='latest', device='cuda:0'):
     dataset_config = load_config(*loadpath, 'dataset_config.pkl')
@@ -57,4 +64,202 @@ def load_diffusion(*loadpath, epoch='latest', device='cuda:0'):
 
     trainer.load(epoch)
 
+    return DiffusionExperiment(dataset, renderer, model, diffusion, trainer.ema_model, trainer, epoch)
+
+
+def load_dataset_config(config, val=False):
+    dataset_config = Config(
+        config.loader,
+        savepath='dataset_config.pkl' if not val else 'dataset_config_val.pkl',
+        env=config.dataset if not val else config.dataset_val,
+        horizon=config.horizon,
+        normalizer=config.normalizer,
+        preprocess_fns=config.preprocess_fns,
+        use_padding=config.use_padding,
+        max_path_length=config.max_path_length,
+        condition_indices=config.condition_indices,
+        include_returns=config.include_returns,
+        returns_scale=config.returns_scale,
+        discount=config.discount,
+        termination_penalty=config.termination_penalty,
+        repres=config.representation
+    )
+    return dataset_config
+
+
+def load_renderer_config(config):
+    render_config = Config(
+        config.renderer,
+        savepath='render_config.pkl',
+        env=config.dataset,
+        repres=config.representation
+    )
+    return render_config
+
+
+def load_model_config(config, obs_dim, act_dim, ret_dim):
+    if config.diffusion == 'models.GaussianInvDynDiffusion' or config.diffusion == 'models.SE3Diffusion':
+        model_config = Config(
+            config.model,
+            savepath='model_config.pkl',
+            horizon=config.horizon,
+            transition_dim=obs_dim,
+            returns_dim=ret_dim,
+            cond_dim=obs_dim,
+            dim_mults=config.dim_mults,
+            returns_condition=config.returns_condition,
+            dim=config.dim,
+            condition_dropout=config.condition_dropout,
+            calc_energy=config.calc_energy,
+            device=torch.device(config.device),
+        )
+    else:
+        model_config = Config(
+            config.model,
+            savepath='model_config.pkl',
+            horizon=config.horizon,
+            transition_dim=obs_dim + act_dim,
+            returns_dim=ret_dim,
+            cond_dim=obs_dim,
+            dim_mults=config.dim_mults,
+            returns_condition=config.returns_condition,
+            dim=config.dim,
+            condition_dropout=config.condition_dropout,
+            calc_energy=config.calc_energy,
+            device=torch.device(config.device),
+        )
+    return model_config
+
+
+def load_diffuser_config(config, obs_dim, act_dim):
+    if config.diffusion == 'models.GaussianInvDynDiffusion':
+        diffusion_config = Config(
+            config.diffusion,
+            savepath='diffusion_config.pkl',
+            horizon=config.horizon,
+            observation_dim=obs_dim,
+            action_dim=act_dim,
+            n_diffsteps=config.n_diffusion_steps,
+            loss_type=config.loss_type,
+            clip_denoised=config.clip_denoised,
+            predict_epsilon=config.predict_epsilon,
+            hidden_dim=config.hidden_dim,
+            ar_inv=config.ar_inv,
+            train_only_inv=config.train_only_inv,
+            ## loss weighting
+            action_weight=config.action_weight,
+            loss_weights=config.loss_weights,
+            loss_discount=config.loss_discount,
+            returns_condition=config.returns_condition,
+            condition_guidance_w=config.condition_guidance_w,
+            # Kinematic loss
+            train_kinematic_loss=config.train_kinematic_loss,
+            kinematic_loss_type=config.kinematic_loss_type,
+            kinematic_scale=config.kinematic_scale,
+            max_kin_weight=config.max_kin_weight,
+            kin_weight_cutoff=config.kin_weight_cutoff,
+            dt=config.dt,
+            pose_only=config.pose_only,
+            train_data_loss=config.train_data_loss,
+            device=torch.device(config.device),
+        )
+    elif config.diffusion == 'models.SE3Diffusion':
+        diffusion_config = Config(
+            config.diffusion,
+            savepath='diffusion_config.pkl',
+            horizon=config.horizon,
+            observation_dim=obs_dim,
+            action_dim=act_dim,
+            n_diffsteps=config.n_diffusion_steps,
+            loss_type=config.loss_type,
+            clip_denoised=config.clip_denoised,
+            predict_epsilon=config.predict_epsilon,
+            hidden_dim=config.hidden_dim,
+            ar_inv=config.ar_inv,
+            train_only_inv=config.train_only_inv,
+            # noise scaling
+            gamma=config.gamma,
+            # loss weighting
+            action_weight=config.action_weight,
+            loss_weights=config.loss_weights,
+            loss_discount=config.loss_discount,
+            returns_condition=config.returns_condition,
+            condition_guidance_w=config.condition_guidance_w,
+            # Kinematic loss
+            kinematic_loss=config.kinematic_loss,
+            kinematic_scale=config.kinematic_scale,
+            max_kin_weight=config.max_kin_weight,
+            dt=config.dt,
+            device=torch.device(config.device),
+        )
+    else:
+        diffusion_config = Config(
+            config.diffusion,
+            savepath='diffusion_config.pkl',
+            horizon=config.horizon,
+            observation_dim=obs_dim,
+            action_dim=act_dim,
+            n_diffsteps=config.n_diffusion_steps,
+            loss_type=config.loss_type,
+            clip_denoised=config.clip_denoised,
+            predict_epsilon=config.predict_epsilon,
+            ## loss weighting
+            action_weight=config.action_weight,
+            loss_weights=config.loss_weights,
+            loss_discount=config.loss_discount,
+            returns_condition=config.returns_condition,
+            condition_guidance_w=config.condition_guidance_w,
+            # Kinematic loss
+            kinematic_loss=config.kinematic_loss,
+            kinematic_scale=config.kinematic_scale,
+            max_kin_weight=config.max_kin_weight,
+            dt=config.dt,
+            device=torch.device(config.device),
+        )
+    return diffusion_config
+
+
+def load_trainer_config(config):
+    trainer_config = Config(
+        Trainer,
+        savepath='trainer_config.pkl',
+        train_batch_size=config.batch_size,
+        train_lr=config.learning_rate,
+        gradient_accumulate_every=config.gradient_accumulate_every,
+        ema_decay=config.ema_decay,
+        sample_freq=config.sample_freq,
+        save_freq=config.save_freq,
+        log_freq=config.log_freq,
+        label_freq=int(config.n_train_steps // config.n_saves),
+        save_parallel=config.save_parallel,
+        bucket=config.bucket,
+        n_reference=config.n_reference,
+        inference_returns=config.inference_returns,
+        inference_horizon=config.inference_horizon,
+        train_device=torch.device(config.device),
+        save_checkpoints=config.save_checkpoints,
+        val_batch_size=config.val_batch_size,
+        val_nr_batch=config.val_nr_batch,
+    )
+    return trainer_config
+
+
+def load_diffusion_from_config(config, loadpath):
+    dataset = {'train': load_dataset_config(config)(), 'val': None}
+    if hasattr(config, 'dataset_val') and config.dataset_val is not None:
+        dataset['val'] = load_dataset_config(config, val=True)()
+    normalizer = dataset['train'].normalizer
+    obs_dim = dataset['train'].observation_dim
+    act_dim = dataset['train'].action_dim
+    ret_dim = dataset['train'].returns_dim
+    renderer = load_renderer_config(config)()
+    model = load_model_config(config, obs_dim, act_dim, ret_dim)()
+    diffusion = load_diffuser_config(config, obs_dim, act_dim)(model, normalizer=normalizer)
+    trainer = load_trainer_config(config)(diffusion, dataset, renderer, dataset['val'])
+
+    state = torch.load(loadpath)
+    diffusion.load_state_dict(state['model'])
+    trainer.ema_model.load_state_dict(state['ema'])
+    trainer.step = state['step']
+    epoch = -1
     return DiffusionExperiment(dataset, renderer, model, diffusion, trainer.ema_model, trainer, epoch)
--- scripts/train.py
+++ scripts/train.py
@@ -1,6 +1,7 @@
 import diffuser.utils as utils
 import torch
 
+
 def main(**deps):
     from config.locomotion_config import Config
 
@@ -12,46 +13,9 @@ def main(**deps):
     # ---------------------------------- dataset ----------------------------------#
     # -----------------------------------------------------------------------------#
 
-    dataset_config = utils.Config(
-        Config.loader,
-        savepath='dataset_config.pkl',
-        env=Config.dataset,
-        horizon=Config.horizon,
-        normalizer=Config.normalizer,
-        preprocess_fns=Config.preprocess_fns,
-        use_padding=Config.use_padding,
-        max_path_length=Config.max_path_length,
-        condition_indices=Config.condition_indices,
-        include_returns=Config.include_returns,
-        returns_scale=Config.returns_scale,
-        discount=Config.discount,
-        termination_penalty=Config.termination_penalty,
-        repres=Config.representation
-    )
-
-    dataset_val_config = utils.Config(
-        Config.loader,
-        savepath='dataset_config.pkl',
-        env=Config.dataset_val,
-        horizon=Config.horizon,
-        normalizer=Config.normalizer,
-        preprocess_fns=Config.preprocess_fns,
-        use_padding=Config.use_padding,
-        max_path_length=Config.max_path_length,
-        condition_indices=Config.condition_indices,
-        include_returns=Config.include_returns,
-        returns_scale=Config.returns_scale,
-        discount=Config.discount,
-        termination_penalty=Config.termination_penalty,
-        repres=Config.representation
-    )
-
-    render_config = utils.Config(
-        Config.renderer,
-        savepath='render_config.pkl',
-        env=Config.dataset,
-        repres=Config.representation
-    )
+    dataset_config = utils.load_dataset_config(Config)
+    dataset_val_config = utils.load_dataset_config(Config, val=True)
+    render_config = utils.load_renderer_config(Config)
 
     dataset = dataset_config()
     dataset_val = dataset_val_config() if Config.dataset_val is not None else None
@@ -63,143 +27,11 @@ def main(**deps):
     # -----------------------------------------------------------------------------#
     # ------------------------------ model & trainer ------------------------------#
     # -----------------------------------------------------------------------------#
-    if Config.diffusion == 'models.GaussianInvDynDiffusion':
-        diffusion_config = utils.Config(
-            Config.diffusion,
-            savepath='diffusion_config.pkl',
-            horizon=Config.horizon,
-            observation_dim=observation_dim,
-            action_dim=action_dim,
-            n_diffsteps=Config.n_diffusion_steps,
-            loss_type=Config.loss_type,
-            clip_denoised=Config.clip_denoised,
-            predict_epsilon=Config.predict_epsilon,
-            hidden_dim=Config.hidden_dim,
-            ar_inv=Config.ar_inv,
-            train_only_inv=Config.train_only_inv,
-            ## loss weighting
-            action_weight=Config.action_weight,
-            loss_weights=Config.loss_weights,
-            loss_discount=Config.loss_discount,
-            returns_condition=Config.returns_condition,
-            condition_guidance_w=Config.condition_guidance_w,
-            # Kinematic loss
-            train_kinematic_loss=Config.train_kinematic_loss,
-            kinematic_loss_type=Config.kinematic_loss_type,
-            kinematic_scale=Config.kinematic_scale,
-            max_kin_weight=Config.max_kin_weight,
-            kin_weight_cutoff=Config.kin_weight_cutoff,
-            dt=Config.dt,
-            pose_only=Config.pose_only,
-            train_data_loss=Config.train_data_loss,
-            device=torch.device(Config.device),
-        )
-    elif Config.diffusion == 'models.SE3Diffusion':
-        diffusion_config = utils.Config(
-            Config.diffusion,
-            savepath='diffusion_config.pkl',
-            horizon=Config.horizon,
-            observation_dim=observation_dim,
-            action_dim=action_dim,
-            n_diffsteps=Config.n_diffusion_steps,
-            loss_type=Config.loss_type,
-            clip_denoised=Config.clip_denoised,
-            predict_epsilon=Config.predict_epsilon,
-            hidden_dim=Config.hidden_dim,
-            ar_inv=Config.ar_inv,
-            train_only_inv=Config.train_only_inv,
-            # noise scaling
-            gamma=Config.gamma,
-            # loss weighting
-            action_weight=Config.action_weight,
-            loss_weights=Config.loss_weights,
-            loss_discount=Config.loss_discount,
-            returns_condition=Config.returns_condition,
-            condition_guidance_w=Config.condition_guidance_w,
-            # Kinematic loss
-            kinematic_loss=Config.kinematic_loss,
-            kinematic_scale=Config.kinematic_scale,
-            max_kin_weight=Config.max_kin_weight,
-            dt=Config.dt,
-            device=torch.device(Config.device),
-        )
-    else:
-        diffusion_config = utils.Config(
-            Config.diffusion,
-            savepath='diffusion_config.pkl',
-            horizon=Config.horizon,
-            observation_dim=observation_dim,
-            action_dim=action_dim,
-            n_diffsteps=Config.n_diffusion_steps,
-            loss_type=Config.loss_type,
-            clip_denoised=Config.clip_denoised,
-            predict_epsilon=Config.predict_epsilon,
-            ## loss weighting
-            action_weight=Config.action_weight,
-            loss_weights=Config.loss_weights,
-            loss_discount=Config.loss_discount,
-            returns_condition=Config.returns_condition,
-            condition_guidance_w=Config.condition_guidance_w,
-            # Kinematic loss
-            kinematic_loss=Config.kinematic_loss,
-            kinematic_scale=Config.kinematic_scale,
-            max_kin_weight=Config.max_kin_weight,
-            dt=Config.dt,
-            device=torch.device(Config.device),
-        )
+    diffusion_config = utils.load_diffuser_config(Config, observation_dim, action_dim)
 
-    if Config.diffusion == 'models.GaussianInvDynDiffusion' or Config.diffusion == 'models.SE3Diffusion':
-        model_config = utils.Config(
-            Config.model,
-            savepath='model_config.pkl',
-            horizon=Config.horizon,
-            transition_dim=observation_dim,
-            returns_dim=returns_dim,
-            cond_dim=observation_dim,
-            dim_mults=Config.dim_mults,
-            returns_condition=Config.returns_condition,
-            dim=Config.dim,
-            condition_dropout=Config.condition_dropout,
-            calc_energy=Config.calc_energy,
-            device=torch.device(Config.device),
-        )
-    else:
-        model_config = utils.Config(
-            Config.model,
-            savepath='model_config.pkl',
-            horizon=Config.horizon,
-            transition_dim=observation_dim + action_dim,
-            returns_dim=returns_dim,
-            cond_dim=observation_dim,
-            dim_mults=Config.dim_mults,
-            returns_condition=Config.returns_condition,
-            dim=Config.dim,
-            condition_dropout=Config.condition_dropout,
-            calc_energy=Config.calc_energy,
-            device=torch.device(Config.device),
-        )
+    model_config = utils.load_model_config(Config, observation_dim, action_dim, returns_dim)
 
-    trainer_config = utils.Config(
-        utils.Trainer,
-        savepath='trainer_config.pkl',
-        train_batch_size=Config.batch_size,
-        train_lr=Config.learning_rate,
-        gradient_accumulate_every=Config.gradient_accumulate_every,
-        ema_decay=Config.ema_decay,
-        sample_freq=Config.sample_freq,
-        save_freq=Config.save_freq,
-        log_freq=Config.log_freq,
-        label_freq=int(Config.n_train_steps // Config.n_saves),
-        save_parallel=Config.save_parallel,
-        bucket=Config.bucket,
-        n_reference=Config.n_reference,
-        inference_returns=Config.inference_returns,
-        inference_horizon=Config.inference_horizon,
-        train_device=torch.device(Config.device),
-        save_checkpoints=Config.save_checkpoints,
-        val_batch_size=Config.val_batch_size,
-        val_nr_batch=Config.val_nr_batch,
-    )
+    trainer_config = utils.load_trainer_config(Config)
 
     # -----------------------------------------------------------------------------#
     # -------------------------------- instantiate --------------------------------#
